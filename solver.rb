c7d111cd6d6d1fb786fc2c7ae820f74c555a4124
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-03 19:23:20 +0530

    fix linter error

--- solver.rb
+++ solver.rb
@@ -1,7 +1,7 @@
 class Solver
   def factorial(num)
     raise ArgumentError, 'Factorial is not defined for negative integers.' if num.negative?
-    return 1 if n.zero?
+    return 1 if num.zero?
 
     result = 1
     (1..num).each { |i| result *= i }
